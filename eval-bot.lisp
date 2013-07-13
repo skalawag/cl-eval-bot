b63338dadd63282b0e324faef8bb9c90cddf59e9
mark scala <markscala@gmail.com>
2013-07-13 14:51:10 -0500

    delete target

--- eval-bot.lisp
+++ eval-bot.lisp
@@ -316,7 +316,7 @@
   (let* ((res (rot13 (car args)))
 	 (msg (make-instance 'client-privmsg
 			     :target "#amarillolinux"
-			     :contents (bot-message res target))))
+			     :contents (bot-message res))))
     (send :terminal msg)
     (queue-add (send-queue client) msg)))
 
